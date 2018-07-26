4981045d83e1bf331b1864de660c64e455669d34
arre1908 <arredoluis@gmail.com>
2018-07-25 21:10:12 -0500

    react components with modules. separated to components files

--- src/index.js
+++ src/index.js
@@ -1,28 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-const Header = ({ message }) => {
-  return (
-    <h2 className="text-center">
-      {message}
-    </h2>
-  );
-};
-
-Header.propTypes = {
-  message: React.PropTypes.string
-};
-
-const App = () => {
-  return (
-    <div>
-      <Header message="Naming Contests" />
-      <div>
-        ...
-      </div>
-    </div>
-  );
-};
+import App from './components/App';
 
 ReactDom.render(
   <App />,
