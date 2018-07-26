91ba79267a35ed959039dc6e572759c9fc2e3efd
arre1908 <arredoluis@gmail.com>
2018-07-25 20:35:46 -0500

    react components with props

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+const App = (props) => {
+  return (
+    <h2 className="text-center">
+      {props.headerMessage}
+    </h2>
+  );
+};
+
+App.propTypes = {
+  headerMessage: React.PropTypes.string
+};
+
+App.defaultProps = {
+  headerMessage: 'Hello!!'
+};
+
 ReactDom.render(
-  <h2 className="text-center">
-    Hello React with JSX!! -- {Math.random()}
-  </h2>,
+  <App />,
   document.getElementById('root')
 );
