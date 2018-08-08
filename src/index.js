35a6c149caaaf41948483f4929b5069bb543d272
arre1908 <arredoluis@gmail.com>
2018-08-07 19:16:23 -0500

    load json data to App component property

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+import data from './testData';
+console.log(data);
+
 import App from './components/App';
 
 ReactDom.render(
-  <App />,
+  <App contests={data.contests}/>,
   document.getElementById('root')
 );
-
-setTimeout(() => {
-  ReactDom.render(
-    <h2>...</h2>,
-    document.getElementById('root')
-  );
-}, 4000);
