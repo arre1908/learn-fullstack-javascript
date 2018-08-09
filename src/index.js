8c514418d31e3fa60136fd880466186194ad7fd6
arre1908 <arredoluis@gmail.com>
2018-08-08 21:31:58 -0500

    removed second roundtrip to api fetch from react mount

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDOM from 'react-dom';
 
 import App from './components/App';
 
-ReactDom.render(
-  <App initialContests={[]} />,
+ReactDOM.render(
+  <App initialContests={window.initialData.contests} />,
   document.getElementById('root')
 );
